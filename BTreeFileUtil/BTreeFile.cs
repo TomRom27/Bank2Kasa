5e5ecf0fe9f1ddb8ca1d8fea4dc4ad1be16e678b
TomRom27 <tomek.romanowski@gmail.com>
2017-05-10 14:34:34 +0200

    11

--- BTreeFileUtil/BTreeFile.cs
+++ BTreeFileUtil/BTreeFile.cs
@@ -1,16 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BTreeFileUtil
 {
-    public class BTreeFile
+    public class BTreeFile : IDisposable
     {
+        private string filename;
+        private int recSize;
+        private bool useDeleteTag;
+        private bool isOpen;
+        private bool disposed;
+        private byte[] buffer;
+
+
         public BTreeFile(string filename, int recSize, bool useDeleteTag)
+        {
+            this.filename = filename;
+            this.recSize = recSize;
+            this.useDeleteTag = useDeleteTag;
+
+            isOpen = false;
+            disposed = false;
+        }
+
+        public void Open()
         {
 
         }
+
+        public void Add(object o)
+        {
+
+        }
+
+        public object Get(long pos)
+        {
+            return null; // todo
+        }
+
+        public void Close()
+        {
+            if (isOpen)
+            {
+                // todo
+            }
+        }
+
+        private long GetStreamPos(int recNumber)
+        {
+            return 0; // todo
+        }
+
+        private byte[] ObjectToByteArray(Object obj)
+        {
+            if (obj == null)
+                return null;
+
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, obj);
+
+            return ms.ToArray();
+        }
+
+        #region IDisposable related
+        public void Dispose()
+        {
+            Dispose(true);
+
+            // Use SupressFinalize in case a subclass 
+            // of this type implements a finalizer.
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // todo
+                }
+                // Indicate that the instance has been disposed.
+                disposed = true;
+            }
+        }
+        #endregion
+    }
+
+    public class BTreeFileHeader
+    {
+        int FirstFree { get; set; }
+        int I2 { get; set; }
+        int I3 { get; set; }
+        int RecLen { get; set; }
+        int I5 { get; set; }
+        bool AllowDup { get; set; }
+    }
+
+    public class DeleteTagRecord
+    {
+        int NextDeletedPos { get; set; }
+        object Data;
     }
 }
---
+++ WUHelper/Forsa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUHelper
+{
+    [Serializable]
+    public struct Forsa
+    {
+        private const int Denominator = 1000;
+
+        public Forsa(decimal amount)
+        {
+            wuInternal = 0;
+        }
+
+        public decimal Amount
+        {
+            get { return Convert.ToDecimal(wuInternal / Denominator);  }
+            set
+            {
+                wuInternal = Convert.ToInt32(value * Denominator);
+            }
+        }
+
+        private int wuInternal;
+    }
+}
